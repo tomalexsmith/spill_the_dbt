8db2ef8049ae81a6ffdf6604e2a0e05500e6037e
Thomas Smith <thomas.smith@theinformationlab.co.uk>
2024-10-14 17:42:37 +0000

    fix lego.sql

--- models/lego/silver/lego.sql
+++ models/lego/silver/lego.sql
@@ -1,32 +1,20 @@
-with inventory_parts as (
-    select * from {{ source('lego','inventory_parts') }}
-),
-inventories as (
-    select * from {{ source('lego','inventories') }}
-),
-parts as (
-    select * from {{ source('lego','parts') }}
-),
-sets as (
-    select * from {{ source('lego','sets') }}
-),
-themes as (
-    select * from {{ source('lego','themes') }}
-)
+with
+    inventory_parts as (select * from {{ source("lego", "inventory_parts") }}),
+    inventories as (select * from {{ source("lego", "inventories") }}),
+    parts as (select * from {{ source("lego", "parts") }}),
+    sets as (select * from {{ source("lego", "sets") }}),
+    themes as (select * from {{ source("lego", "themes") }})
 
-SELECT 
-	T.name as theme_name,
-	S.name as set_name,
-	S.year as set_year,
-	CASE 
-		WHEN UP.part_num IS NULL THEN 'Not Unique' 
-		ELSE 'Unique' 
-	END as unique_part,
-	COUNT(P.part_num) as parts
-FROM parts as P
-LEFT JOIN {{ ref('unique_parts') }} as UP on P.part_num = UP.part_num
-INNER JOIN inventory_parts as IP on P.part_num = IP.part_number
-INNER JOIN inventories as I on I.id = IP.inventory_id
-INNER JOIN sets as S on S.set_num = I.set_num
-INNER JOIN themes as T on T.id = S.theme_id
-GROUP BY 1,2,3,4
+select
+    t.name as theme_name,
+    s.name as set_name,
+    s.year as set_year,
+    case when up.part_num is null then 'Not Unique' else 'Unique' end as unique_part,
+    count(p.part_num) as parts
+from parts as p
+left join {{ ref("unique_parts") }} as up on p.part_num = up.part_num
+inner join inventory_parts as ip on p.part_num = ip.part_number
+inner join inventories as i on i.id = ip.inventory_id
+inner join sets as s on s.set_num = i.set_num
+inner join themes as t on t.id = s.theme_id
+group by 1, 2, 3, 4
